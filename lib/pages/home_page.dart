4fa42630ddfeee193932fa171fe71f2238cd530c
kathyLopezSMOKE <Katherine Lopez>
2023-10-13 07:25:10 -0500

    drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,63 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Shared Preference"),
       ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage("https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg"),
+                ),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.amber,
+                      backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg",
+                      ),
+                    ),
+                    Text(
+                      "Fiorela Rojas Padilla",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Administrador",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.people_alt),
+              title: Text(
+                "Mi perfil",
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("Hola"),
+        ],
+      ),
     );
   }
 }
